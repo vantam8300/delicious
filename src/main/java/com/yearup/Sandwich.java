c91b00d615029ddd4363d62574f32f4d5f4c1e89
Van Tam Chau <vantam8300@gmail.com>
2024-11-07 23:45:29 -0800

    create Sandwich class

---
+++ src/main/java/com/yearup/Sandwich.java
@@ -0,0 +1,71 @@
+package com.yearup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sandwich {
+    private String size;
+    private String bread;
+    private double price;
+    private List<Topping> toppings;
+    private boolean isToasted;
+
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    public Sandwich(String size, String bread, boolean isToasted) {
+        this.size = size;
+        this.bread = bread;
+        this.isToasted = isToasted;
+        toppings = new ArrayList<>();
+    }
+
+    public Sandwich(String size, String bread, double price, List<Topping> toppings, boolean isToasted) {
+        this.size = size;
+        this.bread = bread;
+        this.price = price;
+        this.toppings = toppings;
+        this.isToasted = isToasted;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<Topping> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(List<Topping> toppings) {
+        this.toppings = toppings;
+    }
+
+    public boolean isToasted() {
+        return isToasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        isToasted = toasted;
+    }
+}
